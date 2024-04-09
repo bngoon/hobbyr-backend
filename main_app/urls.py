a6507f83507814029ee8ce963390d10f343b2035
Booker Ngoon <bookerngoon@gmail.com>
2024-04-09 13:31:38 -0400

    routes tested for verification

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Home, FavoriteList,FollowerList, CreateUserView, LoginView, VerifyUserView, ProjectList, ProjectDetail, UserProfileList, UserProfileDetail, CommentList, AddCommentToProject, RemoveCommentFromProject,AddProjectToFavorite, RemoveProjectFromFavorite, AddFollowerToUserProfile, RemoveFollowerFromUserProfile
+from .views import Home, FavoriteList, FollowerList, CreateUserView, LoginView, VerifyUserView, ProjectList, ProjectDetail, UserProfileList, UserProfileDetail, CommentList, AddCommentToProject, RemoveCommentFromProject, AddProjectToFavorite, RemoveProjectFromFavorite, AddFollowerToUserProfile, RemoveFollowerFromUserProfile
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
@@ -22,19 +22,19 @@ urlpatterns = [
          AddCommentToProject.as_view(), name='add-comment'),
     path('projects/<int:project_id>/remove_comment/<int:comment_id>/',
          RemoveCommentFromProject.as_view(), name='remove-comment'),
-     
 
-     path('followers/', FollowerList.as_view(), name='followers'),
-     path('userprofiles/<int:userprofile_id>/add_follower/<int:follower_id>/',
-          AddFollowerToUserProfile.as_view(), name='add-follower'),
-     path('userprofiles/<int:userprofile_id>/remove_follower/<int:follower_id>/',
-          RemoveFollowerFromUserProfile.as_view(), name='remove-follower'),
 
-     path('favorites/', FavoriteList.as_view(), name='favorites'),
-     path('projects/<int:project_id>/add_favorites/<int:favorite_id>/',
-          AddProjectToFavorite.as_view(), name='add-favorite'),
-     path('projects/<int:project_id>/remove_favorites/<int:favorite_id/',
-          RemoveProjectFromFavorite.as_view(), name='remove-favorite'),
+    path('followers/', FollowerList.as_view(), name='followers'),
+    path('userprofiles/<int:userprofile_id>/add_follower/<int:follower_id>/',
+         AddFollowerToUserProfile.as_view(), name='add-follower'),
+    path('userprofiles/<int:userprofile_id>/remove_follower/<int:follower_id>/',
+         RemoveFollowerFromUserProfile.as_view(), name='remove-follower'),
+
+    path('favorites/', FavoriteList.as_view(), name='favorites'),
+    path('projects/<int:project_id>/add_favorites/<int:favorite_id>/',
+         AddProjectToFavorite.as_view(), name='add-favorite'),
+    path('projects/<int:project_id>/remove_favorites/<int:favorite_id>/',
+         RemoveProjectFromFavorite.as_view(), name='remove-favorite'),
 
 
 
